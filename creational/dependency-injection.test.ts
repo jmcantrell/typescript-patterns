1e218d431ea8ffa73df48e84c85070c476600e9d
Jeremy Cantrell <jmcantrell@gmail.com>
2023-03-22 14:23:18 -0500

    Add dependency injection pattern

---
+++ creational/dependency-injection.test.ts
@@ -0,0 +1,33 @@
+import { Logger, Service } from "./dependency-injection.ts";
+import { assertEquals } from "std/testing/asserts.ts";
+
+class MockLogger implements Logger {
+  entries: Array<[string, string]>;
+
+  constructor() {
+    this.entries = [];
+  }
+
+  info(text: string) {
+    this.entries.push(["info", text]);
+  }
+
+  warn(text: string) {
+    this.entries.push(["warn", text]);
+  }
+
+  error(text: string) {
+    this.entries.push(["error", text]);
+  }
+}
+
+Deno.test("Service", () => {
+  const logger = new MockLogger();
+  const service = new Service({ logger });
+  service.run();
+  assertEquals(logger.entries, [
+    ["info", "did a thing"],
+    ["warn", "did a questionable thing"],
+    ["error", "did a dangerous thing"],
+  ]);
+});
---
+++ creational/dependency-injection.ts
@@ -0,0 +1,37 @@
+export interface Logger {
+  info: (text: string) => void;
+  warn: (text: string) => void;
+  error: (text: string) => void;
+}
+
+export class DefaultLogger implements Logger {
+  log(prefix: string, text: string): void {
+    console.log(`${new Date()}: ${prefix.toUpperCase()}: ${text}`);
+  }
+
+  info(text: string): void {
+    this.log("info", text);
+  }
+
+  warn(text: string): void {
+    this.log("warn", text);
+  }
+
+  error(text: string): void {
+    this.log("error", text);
+  }
+}
+
+export class Service {
+  #logger: Logger;
+
+  constructor(options: { logger?: Logger } = {}) {
+    this.#logger = options.logger || new DefaultLogger();
+  }
+
+  run(): void {
+    this.#logger.info("did a thing");
+    this.#logger.warn("did a questionable thing");
+    this.#logger.error("did a dangerous thing");
+  }
+}
