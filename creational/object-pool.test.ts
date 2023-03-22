5b571e822ff6d0d2520939f0d0a487fdf45cb063
Jeremy Cantrell <jmcantrell@gmail.com>
2023-03-22 15:37:32 -0500

    Add object pool pattern

---
+++ creational/object-pool.test.ts
@@ -0,0 +1,44 @@
+import { ObjectPool } from "./object-pool.ts";
+import {
+  assertEquals,
+  assertMatch,
+  assertStrictEquals,
+  assertThrows,
+} from "std/testing/asserts.ts";
+import { assertSpyCalls, spy } from "std/testing/mock.ts";
+
+Deno.test("ObjectPool", () => {
+  const max_items = 3;
+  const create = spy((id: number) => `object-${id}`);
+  const pool = new ObjectPool(max_items, create);
+
+  assertSpyCalls(create, max_items);
+
+  assertEquals(pool.acquired, 0);
+  assertEquals(pool.available, max_items);
+
+  const validObject = /^object-\d+$/;
+
+  const items = [];
+
+  for (let i = 0; i < max_items; i++) {
+    const item = pool.acquire()!;
+
+    assertMatch(item, validObject);
+    items.push(item);
+
+    assertEquals(pool.acquired, items.length);
+    assertEquals(pool.available, max_items - items.length);
+  }
+
+  assertStrictEquals(pool.acquire(), null);
+
+  // Valid object, but an id that does not belong to the pool.
+  assertThrows(() => pool.release(`object-${max_items}`));
+
+  while (items.length > 0) {
+    pool.release(items.pop()!);
+    assertEquals(pool.acquired, items.length);
+    assertEquals(pool.available, max_items - items.length);
+  }
+});
---
+++ creational/object-pool.ts
@@ -0,0 +1,49 @@
+export type Creator<T> = (id: number) => T;
+
+export interface Lease<T> {
+  value: T;
+  release: () => void;
+}
+
+export class ObjectPool<T> {
+  #items: Set<T>;
+  #available: Array<T>;
+  #acquired: Set<T>;
+
+  constructor(max_items: number, create: Creator<T>) {
+    this.#items = new Set();
+    this.#acquired = new Set();
+    this.#available = [];
+
+    for (let id = 0; id < max_items; id++) {
+      const item = create(id);
+      this.#items.add(item);
+      this.#available.push(item);
+    }
+  }
+
+  get available(): number {
+    return this.#available.length;
+  }
+
+  get acquired(): number {
+    return this.#acquired.size;
+  }
+
+  acquire(): T | null {
+    if (this.#available.length == 0) {
+      return null;
+    }
+    const item = this.#available.shift()!;
+    this.#acquired.add(item);
+    return item;
+  }
+
+  release(item: T) {
+    if (!this.#items.has(item)) {
+      throw new Error(`invalid item: ${item}`);
+    }
+    this.#acquired.delete(item);
+    this.#available.push(item);
+  }
+}
