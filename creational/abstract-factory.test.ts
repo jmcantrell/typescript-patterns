43fbbcb083ad9a9726168f606b33260ea91dfac0
Jeremy Cantrell <jmcantrell@gmail.com>
2023-03-22 14:22:18 -0500

    Add abstract factory pattern

---
+++ creational/abstract-factory.test.ts
@@ -0,0 +1,34 @@
+import { HtmlFactory, MarkdownFactory } from "./abstract-factory.ts";
+import { assertEquals } from "std/testing/asserts.ts";
+
+Deno.test("HtmlFactory", () => {
+  const factory = new HtmlFactory();
+  assertEquals(
+    factory.createLink("My Cool Site", "http://cool.io/~me").render(),
+    '<a href="http://cool.io/~me">My Cool Site</a>',
+  );
+  assertEquals(
+    factory.createHeading(0, "My Cool Page").render(),
+    "<h1>My Cool Page</h1>",
+  );
+  assertEquals(
+    factory.createHeading(1, "About Me").render(),
+    "<h2>About Me</h2>",
+  );
+});
+
+Deno.test("MarkdownFactory", () => {
+  const factory = new MarkdownFactory();
+  assertEquals(
+    factory.createLink("My Cool Site", "http://cool.io/~me").render(),
+    "[My Cool Site](http://cool.io/~me)",
+  );
+  assertEquals(
+    factory.createHeading(0, "My Cool Page").render(),
+    "= My Cool Page",
+  );
+  assertEquals(
+    factory.createHeading(1, "About Me").render(),
+    "== About Me",
+  );
+});
---
+++ creational/abstract-factory.ts
@@ -0,0 +1,95 @@
+export interface Component {
+  render: () => string;
+}
+
+export interface Link extends Component {
+  label: string;
+  target: string;
+}
+
+export interface Heading extends Component {
+  depth: number;
+  text: string;
+}
+
+export interface ComponentFactory {
+  createLink: (label: string, target: string) => Link;
+  createHeading: (level: number, text: string) => Heading;
+}
+
+export class MarkdownLink implements Link {
+  label: string;
+  target: string;
+
+  constructor(label: string, target: string) {
+    this.label = label;
+    this.target = target;
+  }
+
+  render(): string {
+    return `[${this.label}](${this.target})`;
+  }
+}
+
+export class MarkdownHeading implements Heading {
+  depth: number;
+  text: string;
+
+  constructor(depth: number, text: string) {
+    this.depth = depth;
+    this.text = text;
+  }
+
+  render(): string {
+    return `${"=".repeat(this.depth + 1)} ${this.text}`;
+  }
+}
+
+export class MarkdownFactory implements ComponentFactory {
+  createLink(label: string, target: string): Link {
+    return new MarkdownLink(label, target);
+  }
+
+  createHeading(depth: number, text: string): Heading {
+    return new MarkdownHeading(depth, text);
+  }
+}
+
+export class HtmlLink implements Link {
+  label: string;
+  target: string;
+
+  constructor(label: string, target: string) {
+    this.label = label;
+    this.target = target;
+  }
+
+  render(): string {
+    return `<a href="${this.target}">${this.label}</a>`;
+  }
+}
+
+export class HtmlHeading implements Heading {
+  depth: number;
+  text: string;
+
+  constructor(depth: number, text: string) {
+    this.depth = depth;
+    this.text = text;
+  }
+
+  render(): string {
+    const n = this.depth + 1;
+    return `<h${n}>${this.text}</h${n}>`;
+  }
+}
+
+export class HtmlFactory implements ComponentFactory {
+  createLink(label: string, target: string): Link {
+    return new HtmlLink(label, target);
+  }
+
+  createHeading(depth: number, text: string): Heading {
+    return new HtmlHeading(depth, text);
+  }
+}
