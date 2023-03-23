098bc62c3cbde330280f24b0415e00f764734615
Jeremy Cantrell <jmcantrell@gmail.com>
2023-03-23 00:06:10 -0500

    Add object adapter pattern

---
+++ creational/object-adapter.test.ts
@@ -0,0 +1,23 @@
+import { Config, JsonConfig, MapConfig } from "./object-adapter.ts";
+import { assertEquals } from "std/testing/asserts.ts";
+
+function testConfig(config: Config) {
+  assertEquals(config.get("foo"), "a");
+  assertEquals(config.get("bar"), "b");
+  assertEquals(config.get("baz"), "c");
+  assertEquals(config.get("qux"), undefined);
+  config.set("qux", "d");
+  assertEquals(config.get("qux"), "d");
+}
+
+Deno.test("JsonConfig", () => {
+  testConfig(new JsonConfig('{"foo":"a","bar":"b","baz":"c"}'));
+});
+
+Deno.test("MapConfig", () => {
+  const map = new Map<string, string>();
+  map.set("foo", "a");
+  map.set("bar", "b");
+  map.set("baz", "c");
+  testConfig(new MapConfig(map));
+});
---
+++ creational/object-adapter.ts
@@ -0,0 +1,36 @@
+export interface Config {
+  get: (key: string) => string | undefined;
+  set: (key: string, value: string) => void;
+}
+
+export class JsonConfig implements Config {
+  #data: Record<string, string>;
+
+  constructor(s: string) {
+    this.#data = JSON.parse(s);
+  }
+
+  get(key: string): string | undefined {
+    return this.#data[key];
+  }
+
+  set(key: string, value: string) {
+    this.#data[key] = value;
+  }
+}
+
+export class MapConfig implements Config {
+  #data: Map<string, string>;
+
+  constructor(map: Map<string, string>) {
+    this.#data = map;
+  }
+
+  get(key: string): string | undefined {
+    return this.#data.get(key);
+  }
+
+  set(key: string, value: string) {
+    this.#data.set(key, value);
+  }
+}
