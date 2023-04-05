fc211c7ec5dad85300a56173a0ddd3894f84e0cc
Jeremy Cantrell <jmcantrell@gmail.com>
2023-04-05 15:37:29 -0500

    Add module pattern

---
+++ structural/module.test.ts
@@ -0,0 +1,46 @@
+import { createTransformingMultiplexer } from "./module.ts";
+import { assertEquals } from "std/testing/asserts.ts";
+import { assertSpyCall, assertSpyCalls, spy } from "std/testing/mock.ts";
+
+Deno.test("createTransformingMultiplexer", () => {
+  const transform_spy = spy((input: number) => (input * 2).toString());
+  const multiplexer = createTransformingMultiplexer(transform_spy);
+
+  multiplexer.publish(3);
+  assertSpyCalls(transform_spy, 0);
+
+  const num_subscribers = 3;
+  const subscriber_spies = [];
+  const unsubscribes = [];
+
+  for (let i = 0; i < num_subscribers; i++) {
+    const subscriber_spy = spy();
+    subscriber_spies.push(subscriber_spy);
+    unsubscribes.push(multiplexer.subscribe(subscriber_spy));
+  }
+
+  assertEquals(subscriber_spies.length, num_subscribers);
+  assertEquals(unsubscribes.length, num_subscribers);
+
+  multiplexer.publish(3);
+  assertSpyCalls(transform_spy, 1);
+
+  for (const subscriber_spy of subscriber_spies) {
+    assertSpyCalls(subscriber_spy, 1);
+    assertSpyCall(subscriber_spy, 0, { args: ["6"] });
+  }
+
+  unsubscribes[0]();
+
+  multiplexer.publish(4);
+  assertSpyCalls(transform_spy, 2);
+
+  // This one is unsubscribed, so should not have been called.
+  assertSpyCalls(subscriber_spies[0], 1);
+
+  // The rest should have been called, though.
+  for (let i = 1; i < num_subscribers; i++) {
+    assertSpyCalls(subscriber_spies[i], 2);
+    assertSpyCall(subscriber_spies[i], 1, { args: ["8"] });
+  }
+});
---
+++ structural/module.ts
@@ -0,0 +1,41 @@
+// https://en.wikipedia.org/wiki/Module_pattern
+
+export type Send<Data> = (data: Data) => void;
+export type Publish<Data> = Send<Data>;
+export type Subscriber<Data> = Send<Data>;
+export type Unsubscribe = () => void;
+export type Subscribe<Data> = (subscriber: Subscriber<Data>) => Unsubscribe;
+
+export type Transform<Input, Output> = (input: Input) => Output;
+
+export interface TransformingMultiplexer<Input, Output> {
+  publish: Publish<Input>;
+  subscribe: Subscribe<Output>;
+}
+
+export function createTransformingMultiplexer<
+  Data,
+  Input = Data,
+  Output = Input,
+>(
+  transform: Transform<Input, Output>,
+): TransformingMultiplexer<Input, Output> {
+  const subscribers = new Set<Subscriber<Output>>();
+
+  function publish(data: Input) {
+    if (subscribers.size == 0) return;
+    const transformed_data = transform(data);
+    for (const subscriber of subscribers) {
+      subscriber(transformed_data);
+    }
+  }
+
+  function subscribe(subscriber: Subscriber<Output>): Unsubscribe {
+    subscribers.add(subscriber);
+    return () => {
+      subscribers.delete(subscriber);
+    };
+  }
+
+  return { publish, subscribe };
+}
